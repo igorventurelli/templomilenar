008952b0169cbddde838c3acc39b60b40eca1c5e
igorventurelli <igor.venturelli@me.com>
2018-01-07 22:33:00 -0200

    Adding HomeControllerTest

--- view/src/main/java/br/com/templomilenar/controller/HomeController.java
+++ view/src/main/java/br/com/templomilenar/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
+    @RequestMapping("/")
     @ResponseBody
     public String index() {
         return "funciona multi module templo milenar!";
